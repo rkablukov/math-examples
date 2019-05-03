b5b0ee86c99d1e6c2815a76e757a24e50f93ce88
Роман Каблуков <roof@MacBook-Pro-roof.local>
2019-05-03 12:09:04 +0300

    records

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ from forms import QuestionForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '634a3025bfda715e26133b9fcfbe2b93'
-app.config['ENABLE_MULTIPLICATION'] = str(os.environ.get('ENABLE_MULTIPLICATION')).lower() == 'true'
+app.config['ENABLE_MULTIPLICATION'] = str(
+    os.environ.get('ENABLE_MULTIPLICATION')).lower() == 'true'
 
 
 def get_new_question():
     # задаём новый вопрос
     random.seed()
-    nsign = random.randint(1,2 + app.config['ENABLE_MULTIPLICATION'])
+    nsign = random.randint(1, 2 + app.config['ENABLE_MULTIPLICATION'])
     sign = '+' if nsign == 1 else '-' if nsign == 2 else '*'
     if sign == '*':
         fint = random.randint(0, 9)
@@ -32,8 +33,11 @@ def home():
     n_right_answers = int(request.cookies.get('n_right_answers', 0))
     n_wrong_answers = int(request.cookies.get('n_wrong_answers', 0))
     last_answer_date = request.cookies.get('last_answer_date')
+    n_right_answers_best_result = int(
+        request.cookies.get('n_right_answers_best_result', 30))
     now = datetime.now().strftime("%Y-%m-%d")
     if now != last_answer_date:
+        n_right_answers_best_result = n_right_answers
         n_right_answers = 0
         n_wrong_answers = 0
     last_answer_date = now
@@ -41,10 +45,12 @@ def home():
     form = QuestionForm()
     if form.validate_on_submit():
         if form.answer.data == int(form.right_answer.data):
-            flash(f"Правильно! {form.question.data} = {form.answer.data}", 'success')
+            flash(
+                f"Правильно! {form.question.data} = {form.answer.data}", 'success')
             n_right_answers += 1
         else:
-            flash(Markup(f"Ошибка! {form.question.data} = <strike>{form.answer.data}</strike> {form.right_answer.data}"), 'danger')
+            flash(Markup(
+                f"Ошибка! {form.question.data} = <strike>{form.answer.data}</strike> {form.right_answer.data}"), 'danger')
             n_wrong_answers += 1
         form = get_new_question()
     elif request.method == 'GET':
@@ -53,13 +59,19 @@ def home():
         flash('В ответе должно быть введено число!', 'danger')
 
     image_file = None
-    if n_right_answers >= 30:
-        image_file = url_for('static', filename='good_job_pics/' + str(datetime.now().weekday()) + '.jpg')
+    if n_right_answers >= n_right_answers_best_result:
+        image_file = url_for(
+            'static', filename='good_job_pics/' + str(datetime.now().weekday()) + '.jpg')
 
-    response = make_response(render_template('home.html', form=form, n_right_answers=n_right_answers, n_wrong_answers=n_wrong_answers, image_file=image_file))
+    response = make_response(render_template('home.html', form=form,
+                                             n_right_answers=n_right_answers,
+                                             n_wrong_answers=n_wrong_answers,
+                                             n_right_answers_best_result=n_right_answers_best_result,
+                                             image_file=image_file))
     response.set_cookie('n_right_answers', str(n_right_answers))
     response.set_cookie('n_wrong_answers', str(n_wrong_answers))
     response.set_cookie('last_answer_date', last_answer_date)
+    response.set_cookie('n_right_answers_best_result', str(n_right_answers_best_result))
     return response
 
 
